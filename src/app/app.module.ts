e5cd9c99537288d200e4484e8660c4940b1fd5b6
Matias Matinez <martinezrebori@fpuna.edu.py>
2022-01-28 19:11:35 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MaterialModule} from './material/material.module';
+import { LoginComponent } from './login/login.component'
+/* firebase */
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDOTcuNSXgybC1ho6YAAa_8A6eRXo_uq6I",
+    authDomain: "gym-angular-92b3e.firebaseapp.com",
+    projectId: "gym-angular-92b3e",
+    storageBucket: "gym-angular-92b3e.appspot.com",
+    messagingSenderId: "951291870232",
+    appId: "1:951291870232:web:5e6472c85627927ae4976d",
+    measurementId: "G-1RK2LLNS7Z"
+  }
 };
 
 /*
